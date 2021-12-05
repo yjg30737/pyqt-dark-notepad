9b6d4061221344f707d932aea49035673f281b35
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-05 14:09:10 +0900

    Remove duplicated install-required module from setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     url='https://github.com/yjg30737/pyqt-dark-notepad.git',
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-find-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-text-widget.git@main',
         'pyqt-find-replace-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-replace-text-widget.git@main',
         'pyqt-font-dialog @ git+https://git@github.com/yjg30737/pyqt-font-dialog.git@main',
         'pyqt5-color-dialog @ git+https://git@github.com/yjg30737/pyqt5-color-dialog.git@main'
