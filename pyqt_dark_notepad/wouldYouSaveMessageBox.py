747b842714c5217ef7802dcd7a00cae9941ce906
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-18 11:52:39 +0900

    Deal with style of wouldYouSaveMessageBox.py #2

--- pyqt_dark_notepad/wouldYouSaveMessageBox.py
+++ pyqt_dark_notepad/wouldYouSaveMessageBox.py
@@ -1,5 +1,4 @@
 import os
-
 from PyQt5.QtWidgets import QMessageBox
 from pyqt_resource_helper import PyQtResourceHelper
 
@@ -7,5 +6,4 @@ from pyqt_resource_helper import PyQtResourceHelper
 class WouldYouSaveMessageBox(QMessageBox):
     def __init__(self):
         super().__init__()
-        PyQtResourceHelper.setStyleSheet([self], ['style/dark_gray_theme.css'])
         PyQtResourceHelper.setStyleSheet([self], ['style/no_icon_button.css'])
