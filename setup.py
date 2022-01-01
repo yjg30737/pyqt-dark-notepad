37b57801ace3094b9400bdc1d42738fb55f096a8
Jung Gyu Yoon <yjg30737@gmail.com>
2022-01-01 11:54:02 +0900

    Remove redundant package in install required and requirements.txt (pyqt-resource-helper, already in the pyqt-find-text-widget installed by pyqt-find-replace-text-widget), Update version (0.2.1 -> 0.3.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-dark-notepad',
-    version='0.2.1',
+    version='0.3.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -14,7 +14,6 @@ setup(
         'PyQt5>=5.8',
         'pyqt-find-replace-text-widget @ git+https://git@github.com/yjg30737/pyqt-find-replace-text-widget.git@main',
         'pyqt-font-dialog @ git+https://git@github.com/yjg30737/pyqt-font-dialog.git@main',
-        'pyqt-color-dialog @ git+https://git@github.com/yjg30737/pyqt-color-dialog.git@main',
-        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
+        'pyqt-color-dialog @ git+https://git@github.com/yjg30737/pyqt-color-dialog.git@main'
     ]
 )
