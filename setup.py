281a1f3e69d70ba6e12b708fa5ae1b36001de920
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-04 10:16:44 +0900

    Update version (1.2.0 -> 1.3.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-dark-notepad',
-    version='1.2.0',
+    version='1.3.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
