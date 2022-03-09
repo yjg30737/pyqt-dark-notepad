5f46b6ed2d9b9928d145e981564204f26b7980ef
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-09 17:24:33 +0900

    Apply new package (pyqt-new-window-handler)

--- pyqt_dark_notepad/darkNotepadApp.py
+++ pyqt_dark_notepad/darkNotepadApp.py
@@ -5,36 +5,11 @@ from pyqt_dark_notepad import DarkNotepad
 from pyqt_style_setter import StyleSetter
 from pyqt_custom_titlebar_setter import CustomTitlebarSetter
 from pyqt_custom_titlebar_window import CustomTitlebarWindow
+from pyqt_new_window_handler import NewWindowHandler
+from python_get_absolute_resource_path import get_absolute_resource_path
 
 
 class DarkNotepadApp(QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.installEventFilter(self)
-        self.__windowDict = dict()
-        self.__new()
-
-    def __new(self):
-        mainWindow = DarkNotepad()
-        mainWindow.newClicked.connect(self.__new)
-        StyleSetter.setWindowStyle(mainWindow)
-        titleBarWindow = CustomTitlebarSetter.getCustomTitleBar(mainWindow, icon_filename='ico/dark-notepad.svg')
-        titleBarWindow.setAttribute(Qt.WA_DeleteOnClose)
-        titleBarWindow.destroyed.connect(self.__destroyed)
-        titleBarWindow.show()
-
-    def eventFilter(self, obj, e):
-        if isinstance(obj, CustomTitlebarWindow):
-            # catch the QShowEvent of CustomTitlebarWindow
-            if e.type() == 17:
-                w = self.__getInnerWidget(obj)
-                self.__windowDict[w] = obj
-        return super().eventFilter(obj, e)
-
-    def __destroyed(self, w):
-        w = self.__getInnerWidget(w)
-        del(self.__windowDict[w])
-
-    def __getInnerWidget(self, w):
-        inner_widget = [c for c in w.children() if isinstance(c, DarkNotepad)][0]
-        return inner_widget
+        self.__handler = NewWindowHandler(DarkNotepad, get_absolute_resource_path('ico/dark-notepad.svg'))
