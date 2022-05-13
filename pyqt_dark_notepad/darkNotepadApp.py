b8cf30b2144e90b65fd7cd886a5dab5b81dc5dd2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 08:46:17 +0900

    Apply renamed package

--- pyqt_dark_notepad/darkNotepadApp.py
+++ pyqt_dark_notepad/darkNotepadApp.py
@@ -2,10 +2,10 @@ from PyQt5.QtWidgets import QApplication
 from pyqt_dark_notepad import DarkNotepad
 
 from pyqt_new_window_handler import NewWindowHandler
-from python_get_absolute_resource_path import get_absolute_resource_path
+import absresgetter
 
 
 class DarkNotepadApp(QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__handler = NewWindowHandler(DarkNotepad, get_absolute_resource_path('ico/dark-notepad.svg'))
+        self.__handler = NewWindowHandler(DarkNotepad, absresgetter.getabsres('ico/dark-notepad.svg'))
